868864a5f43deb840be774f8917280e12b23ccd0
Lounis83 <97181618+Lounis83@users.noreply.github.com>
2022-01-08 14:44:20 +0100

    Add files via upload

---
+++ homework.java
@@ -0,0 +1,16 @@
+package datatype;
+
+public class homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub stub
+	String firstName = "Lounis";
+			String lastName="akli";
+	String id = "30390ny";
+	
+	System.out.println("my name is "+firstName+""+lastName+" and this is my id number "+ id );
+	
+			
+		
+
+	}}	
---
+++ project src6.java
@@ -0,0 +1,45 @@
+package datatype;
+
+import java.util.Scanner;
+
+public class tempconverter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+ System.out.println("-------------------------welcome-----------------------------");
+ System.out.println("---celsuis-Fahrenheit/Fahrenheit-celsuis converter------");
+ do {
+	 System.out.prontln("choose the conversion mode:");
+	 System.out.println("1-celsuis-Fahrenheit");java.util.Scanner
+	 System.out.println("2-Farenheit-celsuis ");
+	 
+	 
+	 int x;
+	 double z;
+	 Scanner sc = new Scanner(System,in);
+	 x=sc.nextint();
+	 if (x==1) {
+		 System.out.println("Enter the temperature to convert;");
+		 z=sc.nextDouble();
+	 double c=(z-32) *5/9;
+	 System.out.println(c) ;
+	 }
+	 System.out.println(do you like to convert other temperatures?)
+	 Y/N");
+	 		System.out.println("press 1 for yes or 2 for no");
+ h=sc.nextline();
+ }
+ while (h==0);
+      System.out.println("------------------------------Good bye, see you soon---------------------------");
+      
+      }
+      
+      
+      }
+      
+      
+      
